fcf6b528aa6ab59f9c94433a58bad49382333299
Caio Moura <104685081+CaioMouras@users.noreply.github.com>
2026-03-25 13:27:23 -0300

    mudança no som e no tempo de detecção dos erros.

--- main.ts
+++ main.ts
@@ -5,7 +5,10 @@ basic.forever(function () {
         basic.showNumber(erros)
     } else if (input.pinIsPressed(TouchPin.P0)) {
         basic.showIcon(IconNames.No)
-        basic.pause(1000)
+        music.play(music.tonePlayable(880, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(932, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(988, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+        basic.pause(300)
         erros += 1
         basic.showNumber(erros)
     }
